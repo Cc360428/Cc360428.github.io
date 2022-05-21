7192e94c1f88bc8424c032b264d61ba10cc720fc
Cc <li_chao_cheng@163.com>
2022-05-21 10:23:56 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-self.importScripts('/assets/js/data/swcache.js');
+self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220521.1008';
+const cacheName = 'chirpy-20220521.1000';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
