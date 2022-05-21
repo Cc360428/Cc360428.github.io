b6cf41633239d669c7d149d20cf486ae39a99911
Cc <li_chao_cheng@163.com>
2022-05-21 15:40:24 +0800

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220521.1052';
+const cacheName = 'chirpy-20220521.1539';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
