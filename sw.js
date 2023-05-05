b92c80e66f26b3448b371b7ab2f2bc790c062da3
Cc <li_chao_cheng@163.com>
2023-05-05 18:27:18 +0800

    leetcode

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230505.1818';
+const cacheName = 'chirpy-20230505.1826';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
