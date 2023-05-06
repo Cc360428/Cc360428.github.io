6921404d75f99481ce39066ae53ba8396de06c5d
Cc <li_chao_cheng@163.com>
2023-05-06 15:15:29 +0800

    blog push

---
+++ reset_localhost_blog.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf ./_site ;bundle exec jekyll build
+rm -rf /opt/localhost/nginx/html/blog/*
+mv /opt/code/gitee.com/ClientCodeFile/jekyall_blog/_site/* /opt/localhost/nginx/html/blog/
+
+
