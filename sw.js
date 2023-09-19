53a13d9095fd7a9d11a9c7efa40d582d0add24a1
Cc <li_chao_cheng@163.com>
2023-09-19 21:14:41 +0800

    blog push

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230919.211002';
+const cacheName = 'chirpy-20230919.211439';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
