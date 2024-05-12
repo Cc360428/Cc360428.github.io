92fb8c9d9302286743bfddc12b9cc3305f5d348d
Cc <li_chao_cheng@163.com>
2024-05-12 20:43:03 +0800

    blog push

--- local.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm -rf ./_site
-bundle exec jekyll build
-rm -rf /opt/localhost/nginx/html/blog/*
-mv /opt/code/gitee.com/ClientCodeFile/jekyall_blog/_site/* /opt/localhost/nginx/html/blog/
